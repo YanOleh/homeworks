3ded5570d74f72e01ba218bcecc078baf516b8df
Oleh <o.yankula@gmail.com>
2023-10-23 11:20:47 +0200

    Complete HomeWork-9

--- HomeWork_7/task2.py
+++ HomeWork_7/task2.py
@@ -10,18 +10,9 @@ prices = {
     "orange": 1.5,
     "pear": 3
 }
-#--------------- First variant ----------------------
 
-total_price = {
-    "banana": stock["banana"] * prices["banana"],
-    "apple": stock["apple"] * prices["apple"],
-    "orange": stock["orange"] * prices["orange"],
-    "pear": stock["pear"] * prices["pear"]
-}
-for key, value in total_price.items():
-    print(f"Name product: {key}, Product amount = {value} $")
 
-#--------------- Second variant ---------------------
+
 
 total_price1 = {key1: prices[key1] * stock[key1] for key1 in stock}
 
